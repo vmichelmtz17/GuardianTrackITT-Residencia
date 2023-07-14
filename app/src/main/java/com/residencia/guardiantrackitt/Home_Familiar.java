d627d05c08dd9f93ed55f7a91072fc403e3d0d96
Vicente Michel Martinez Portela <vicente.martinez17@tectijuana.edu.mx>
2023-07-13 20:35:37 -0700

    version044

--- app/src/main/java/com/residencia/guardiantrackitt/Home_Familiar.java
+++ app/src/main/java/com/residencia/guardiantrackitt/Home_Familiar.java
@@ -1,6 +1,7 @@
 package com.residencia.guardiantrackitt;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -16,13 +18,21 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import android.net.Uri;
-
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 
 public class Home_Familiar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private TextView userNameTextView;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
+    private FirebaseUser mCurrentUser;
+    private ListenerRegistration mUserListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +46,48 @@ public class Home_Familiar extends AppCompatActivity implements NavigationView.O
         navigationView = findViewById(R.id.navigationView);
         navigationView.setNavigationItemSelectedListener(this);
 
-
         // Configurar el ActionBarDrawerToggle para abrir y cerrar el NavigationDrawer
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        // Obtener referencias a las vistas
+        userNameTextView = navigationView.getHeaderView(0).findViewById(R.id.userNameTextView);
+
+        // Obtener instancia de FirebaseAuth y FirebaseFirestore
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+
+        // Obtener el usuario actual
+        mCurrentUser = mAuth.getCurrentUser();
+
+        // Verificar si el usuario actual no es nulo
+        if (mCurrentUser != null) {
+            // Obtener el ID del usuario actual
+            String userId = mCurrentUser.getUid();
+
+            // Escuchar cambios en el documento del usuario en Firestore
+            mUserListener = mFirestore.collection("users")
+                    .document(userId)
+                    .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                        @Override
+                        public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                            if (e != null) {
+                                // Manejar el error
+                                return;
+                            }
+
+                            // Verificar si el documento existe y contiene datos
+                            if (documentSnapshot != null && documentSnapshot.exists()) {
+                                // Obtener el nombre del usuario del documento
+                                String userName = documentSnapshot.getString("name");
+
+                                // Mostrar el nombre del usuario en el TextView correspondiente
+                                userNameTextView.setText(userName);
+                            }
+                        }
+                    });
+        }
 
         // Configurar el clic de los botones en el contenido principal
         Button buttonUbicacion = findViewById(R.id.buttonUbicacion);
@@ -60,12 +107,9 @@ public class Home_Familiar extends AppCompatActivity implements NavigationView.O
             @Override
             public void onClick(View v) {
                 String url = "https://alzheimertijuana.netlify.app/";
-
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse(url));
                 startActivity(intent);
-                // Lógica para el botón de página web
-                // Aún no está programado, puedes agregar el código más tarde
             }
         });
 
@@ -74,7 +118,6 @@ public class Home_Familiar extends AppCompatActivity implements NavigationView.O
             public void onClick(View v) {
                 Intent intent = new Intent(Home_Familiar.this, Contacto.class);
                 startActivity(intent);
-                //Codigo para agregar nuevos contactos
             }
         });
 
@@ -86,6 +129,7 @@ public class Home_Familiar extends AppCompatActivity implements NavigationView.O
             }
         });
     }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Maneja los clics en los elementos del menú del NavigationDrawer
@@ -95,7 +139,7 @@ public class Home_Familiar extends AppCompatActivity implements NavigationView.O
                 startActivity(profileIntent);
                 return true;
             case R.id.menu_logout:
-                // Lógica para cerrar sesión
+                // Cerrar sesión y redirigir a la actividad de inicio de sesión
                 FirebaseAuth.getInstance().signOut();
                 Intent logoutIntent = new Intent(Home_Familiar.this, MainActivity.class);
                 startActivity(logoutIntent);
@@ -105,6 +149,7 @@ public class Home_Familiar extends AppCompatActivity implements NavigationView.O
                 return false;
         }
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         // Maneja los clics en el botón de menú de la ActionBar
@@ -122,6 +167,15 @@ public class Home_Familiar extends AppCompatActivity implements NavigationView.O
             drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Detener la escucha de cambios en el documento del usuario en Firestore
+        if (mUserListener != null) {
+            mUserListener.remove();
         }
     }
 }
