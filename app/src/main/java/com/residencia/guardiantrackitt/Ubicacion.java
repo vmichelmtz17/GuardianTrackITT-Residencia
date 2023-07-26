aa8a3f01556d685fed0daa362c34f5102894181d
Vicente Michel Martinez Portela <vicente.martinez17@tectijuana.edu.mx>
2023-07-26 15:20:40 -0700

    UPDATE 03

--- app/src/main/java/com/residencia/guardiantrackitt/Ubicacion.java
+++ app/src/main/java/com/residencia/guardiantrackitt/Ubicacion.java
@@ -1,42 +1,21 @@
 package com.residencia.guardiantrackitt;
 
-import androidx.fragment.app.FragmentManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.residencia.guardiantrackitt.UbicacionFragment;
+public class Ubicacion extends AppCompatActivity {
 
-public class Ubicacion {
-    private double latitude;
-    private double longitude;
-    public Ubicacion(){}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubicacion);
 
-    public Ubicacion(double latitude, double longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
+        // Obtener una instancia del fragmento UbicacionPaciente
+        UbicacionFragment ubicacionPacienteFragment = new UbicacionFragment();
 
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public void mostrarUbicacionFragmentConUbicacionActual(LatLng ubicacionActual, FragmentManager fragmentManager, int fragmentContainerId) {
-        UbicacionFragment ubicacionFragment = new UbicacionFragment();
-        ubicacionFragment.setUbicacionActual(ubicacionActual);
-
-        fragmentManager.beginTransaction()
-                .replace(fragmentContainerId, ubicacionFragment) // fragmentContainerId es el ID del contenedor donde deseas mostrar el fragmento
+        // Agregar el fragmento al contenedor (FrameLayout) definido en activity_ubicacion_paciente.xml
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, ubicacionPacienteFragment)
                 .commit();
     }
 }
